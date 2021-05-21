41f90ab958c8b4b7d1881aea8c5bac7fe75b6174
Dilan Madhusanka <dilanm@insharptechnologies.com>
2021-05-21 17:41:32 +0530

    upload dummy project

---
+++ src/main/java/com/example/fileupload/file_uploader_to_firebase/FileUploaderToFirebaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.fileupload.file_uploader_to_firebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploaderToFirebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploaderToFirebaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fileupload/file_uploader_to_firebase/FileUploaderToFirebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fileupload.file_uploader_to_firebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploaderToFirebaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
